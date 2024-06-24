3d81e1ab6cf514a83c914e3e31d5be29e1397cd1
Preetham John <78245284+pj323@users.noreply.github.com>
2024-06-24 15:46:08 -0700

    Update fetch_data.sh

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Define namespaces and output file names
-NAMESPACE_A="edcr"
-NAMESPACE_B="edco"
-FILE_A="cache-prod-${NAMESPACE_A}-instance-data.json"
-FILE_B="cache-prod-${NAMESPACE_B}-instance-data.json"
+# Command line arguments for namespace, cluster identifier, and Kubernetes context
+NAMESPACE=$1
+CLUSTER=$2
 
-# Fetch stateful set data
-kubectl get sts --namespace $NAMESPACE_A -o json > $FILE_A
-kubectl get sts --namespace $NAMESPACE_B -o json > $FILE_B
+
+# Output file name based on cluster and namespace
+FILE_NAME="${CLUSTER}-${NAMESPACE}.json"
+
+# Fetch stateful set data using kubectl with the specified context
+echo "Fetching stateful sets from $CLUSTER using context $CONTEXT..."
+kubectl get sts --namespace $NAMESPACE -o json > $FILE_NAME
+
+echo "Data successfully written to $FILE_NAME"
+
