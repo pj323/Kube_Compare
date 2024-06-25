92c308261585c0151eb62f6bcc76f5fdb998e437
Preetham John <78245284+pj323@users.noreply.github.com>
2024-06-24 18:20:37 -0700

    Update compare_data.sh

--- scripts/compare_data.sh
+++ scripts/compare_data.sh
@@ -6,18 +6,38 @@ FILE_B="cache-prod-edco-instance-data.json"
 
 # Use jq to compare files and find differences
 jq --argfile a $FILE_A --argfile b $FILE_B -n '
-  [($a.items[] | {name: .metadata.name, replicas: .spec.replicas, containers: .spec.template.spec.containers | map({container_name: .name, resources: .resources})})] as $aList |
-  [($b.items[] | {name: .metadata.name, replicas: .spec.replicas, containers: .spec.template.spec.containers | map({container_name: .name, resources: .resources})})] as $bList |
+  [($a.items[] | {
+    name: .metadata.labels["app.kubernetes.io/name"],
+    replicas: .spec.replicas,
+    containers: .spec.template.spec.containers | map({
+      container_name: .name,
+      cpu_request: .resources.requests.cpu,
+      cpu_limit: .resources.limits.cpu,
+      memory_request: .resources.requests.memory,
+      memory_limit: .resources.limits.memory
+    })
+  })] as $aList |
+  [($b.items[] | {
+    name: .metadata.labels["app.kubernetes.io/name"],
+    replicas: .spec.replicas,
+    containers: .spec.template.spec.containers | map({
+      container_name: .name,
+      cpu_request: .resources.requests.cpu,
+      cpu_limit: .resources.limits.cpu,
+      memory_request: .resources.requests.memory,
+      memory_limit: .resources.limits.memory
+    })
+  })] as $bList |
   {
     in_a_not_in_b: ($aList | map(.name) - $bList | map(.name)),
     in_b_not_in_a: ($bList | map(.name) - $aList | map(.name)),
     diff_replicas: ($aList[] | . as $aItem | $bList[] | select(.name == $aItem.name and .replicas != $aItem.replicas)),
     resource_differences: ($aList[] | . as $aItem | $bList[] | select(.name == $aItem.name) | .containers[] as $bContainer | $aItem.containers[] | select(.container_name == $bContainer.container_name) | {
       container_name: .container_name,
-      cpu_request_diff: (if .resources.requests.cpu != $bContainer.resources.requests.cpu then {a: .resources.requests.cpu, b: $bContainer.resources.requests.cpu} else empty end),
-      memory_request_diff: (if .resources.requests.memory != $bContainer.resources.requests.memory then {a: .resources.requests.memory, b: $bContainer.resources.requests.memory} else empty end),
-      cpu_limit_diff: (if .resources.limits.cpu != $bContainer.resources.limits.cpu then {a: .resources.limits.cpu, b: $bContainer.resources.limits.cpu} else empty end),
-      memory_limit_diff: (if .resources.limits.memory != $bContainer.resources.limits.memory then {a: .resources.limits.memory, b: $bContainer.resources.limits.memory} else empty end)
+      cpu_request_diff: (if .cpu_request != $bContainer.cpu_request then {a: .cpu_request, b: $bContainer.cpu_request} else empty end),
+      memory_request_diff: (if .memory_request != $bContainer.memory_request then {a: .memory_request, b: $bContainer.memory_request} else empty end),
+      cpu_limit_diff: (if .cpu_limit != $bContainer.cpu_limit then {a: .cpu_limit, b: $bContainer.cpu_limit} else empty end),
+      memory_limit_diff: (if .memory_limit != $bContainer.memory_limit then {a: .memory_limit, b: $bContainer.memory_limit} else empty end)
     })
   }
 ' > differences.json
