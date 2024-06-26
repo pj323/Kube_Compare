42ae1f1261b00361c697cbec4103e1f79acb75c7
Preetham John <78245284+pj323@users.noreply.github.com>
2024-06-26 13:08:03 -0700

    Update compare_data.sh

--- scripts/compare_data.sh
+++ scripts/compare_data.sh
@@ -53,21 +53,28 @@ cat differences.json
 
 - |
       jq --argfile a data_${CLUSTER_A_CONTEXT}_${NAMESPACE}.json --argfile b data_${CLUSTER_B_CONTEXT}_${NAMESPACE}.json -n '
+      def safe_compare($fieldA; $fieldB; $label):
+        if $fieldA != $fieldB and ($fieldA | length) > 0 and ($fieldB | length) > 0 then
+          {($label): {"A": $fieldA, "B": $fieldB}}
+        else
+          empty
+        end;
       ($a.items[] | {name: .metadata.name, replicas: .spec.replicas, resources: .spec.template.spec.containers[0].resources}) as $itemsA
       | ($b.items[] | {name: .metadata.name, replicas: .spec.replicas, resources: .spec.template.spec.containers[0].resources}) as $itemsB
       | if $itemsA.name == $itemsB.name then 
         {
           name: $itemsA.name,
-          differences: 
-          {
-            replica_differences: (if $itemsA.replicas != $itemsB.replicas then {A_replicas: $itemsA.replicas, B_replicas: $itemsB.replicas} else empty end),
-            cpu_limits: (if $itemsA.resources.limits.cpu != $itemsB.resources.limits.cpu then {A_cpu_limits: $itemsA.resources.limits.cpu, B_cpu_limits: $itemsB.resources.limits.cpu} else empty end),
-            memory_limits: (if $itemsA.resources.limits.memory != $itemsB.resources.limits.memory then {A_memory_limits: $itemsA.resources.limits.memory, B_memory_limits: $itemsB.resources.limits.memory} else empty end),
-            storage_limits: (if $itemsA.resources.limits["ephemeral-storage"] != $itemsB.resources.limits["ephemeral-storage"] then {A_storage_limits: $itemsA.resources.limits["ephemeral-storage"], B_storage_limits: $itemsB.resources.limits["ephemeral-storage"]} else empty end),
-            cpu_requests: (if $itemsA.resources.requests.cpu != $itemsB.resources.requests.cpu then {A_cpu_requests: $itemsA.resources.requests.cpu, B_cpu_requests: $itemsB.resources.requests.cpu} else empty end),
-            memory_requests: (if $itemsA.resources.requests.memory != $itemsB.resources.requests.memory then {A_memory_requests: $itemsA.resources.requests.memory, B_memory_requests: $itemsB.resources.requests.memory} else empty end),
-            storage_requests: (if $itemsA.resources.requests["ephemeral-storage"] != $itemsB.resources.requests["ephemeral-storage"] then {A_storage_requests: $itemsA.resources.requests["ephemeral-storage"], B_storage_requests: $itemsB.resources.requests["ephemeral-storage"]} else empty end)
-          }
+          differences: [
+            ($itemsA.replicas | tostring) as $repA
+            | ($itemsB.replicas | tostring) as $repB
+            | safe_compare($repA; $repB; "replicas"),
+            safe_compare($itemsA.resources.limits.cpu; $itemsB.resources.limits.cpu; "cpu_limits"),
+            safe_compare($itemsA.resources.limits.memory; $itemsB.resources.limits.memory; "memory_limits"),
+            safe_compare($itemsA.resources.limits["ephemeral-storage"]; $itemsB.resources.limits["ephemeral-storage"]; "storage_limits"),
+            safe_compare($itemsA.resources.requests.cpu; $itemsB.resources.requests.cpu; "cpu_requests"),
+            safe_compare($itemsA.resources.requests.memory; $itemsB.resources.requests.memory; "memory_requests"),
+            safe_compare($itemsA.resources.requests["ephemeral-storage"]; $itemsB.resources.requests["ephemeral-storage"; "storage_requests")
+          ] | add
         }
         else empty
       end' > differences.json
