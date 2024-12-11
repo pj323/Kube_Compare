1977b298f41e08d6cde329130e7306fc96e8054d
Preetham John <78245284+pj323@users.noreply.github.com>
2024-12-10 17:12:23 -0800

    Update Newui.js

--- Newui.js
+++ Newui.js
@@ -22,3 +22,7 @@ import './styles/global.css';
 
 npm install react@18 react-dom@18 react-scripts typescript @testing-library/react @testing-library/user-event web-vitals --legacy-peer-deps
 npm install @mui/material @emotion/react @emotion/styled
+
+
+npm install react-router-dom @types/react-router-dom
+
